8e0e179505c7fd1dad58dc4638449ce0d3830ace
wisemanp <pacelweb@gmail.com>
2025-11-11 17:17:33 +0000

    add import of yaml

--- manager.py
+++ manager.py
@@ -6,6 +6,7 @@ import logging
 import signal
 from typing import List, Optional
 import pathlib
+import yaml
 
 from tides_pipe.modules.classifiers.snid_handler import SnidHandler
 from tides_pipe.utils.paths import spectra_night_dir as util_spectra_night_dir, spectrum_path as util_spectrum_path
